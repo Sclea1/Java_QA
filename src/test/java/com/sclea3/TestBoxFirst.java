cd00409b104ad3848f8b426e7a11dd09ae8760ea
Sclea3 <sclea3@yandex.ru>
2021-12-15 10:57:45 +0300

    TestBox first test

---
+++ src/test/java/com/sclea3/TestBoxFirst.java
@@ -0,0 +1,34 @@
+package com.sclea3;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxFirst {
+
+    @BeforeAll
+    static void config() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+    }
+
+        @Test
+        void fillAllForms() {
+            open("https://demoqa.com/text-box");
+            $("#userName").setValue("Test Name");
+            $("#userEmail").setValue("TestEmail@list.ru");
+            $("#currentAddress").setValue("Test Address");
+            $("#permanentAddress").setValue("Test Permanent Address");
+            $("#submit").click();
+
+            $("#output #name").shouldHave(text("Test Name"));
+            $("#output #email").shouldHave(text("TestEmail@list.ru"));
+            $("#output #currentAddress").shouldHave(text("Test Address"));
+            $("#output #permanentAddress").shouldHave(text("Test Permanent Address"));
+        }
+
+    }
