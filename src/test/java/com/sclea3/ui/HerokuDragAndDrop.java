45a32e137d30ed749b5c45cf639121924f193018
Sclea1 <sclea3@yandex.ru>
2022-01-03 16:24:57 +0300

    Add HerokuDragAndDrop

---
+++ src/test/java/com/sclea3/ui/HerokuDragAndDrop.java
@@ -0,0 +1,27 @@
+package com.sclea3.ui;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HerokuDragAndDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        com.codeborne.selenide.Configuration.browser = "chrome";
+        com.codeborne.selenide.Configuration.browserSize = "1920x1080";
+        Configuration.headless = false;
+    }
+
+    @Test
+    void herokuTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B")).shouldBe(visible);
+        sleep(2000);
+    }
+}
